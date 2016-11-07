3a2a97e7966acbe9914b277c1919252cbff95a50
Carl X. Su <bcbcarl@gmail.com>
2016-11-07 21:55:50 +0800

    Re-format webpack config

--- hello/webpack.config.js
+++ hello/webpack.config.js
@@ -20,7 +20,7 @@ module.exports = {
 
     preLoaders: [
       // Run TSLint before transpiling.
-      {test: /\.tsx?$/, loader: 'tslint' },
+      { test: /\.tsx?$/, loader: 'tslint' },
       // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
       { test: /\.js$/, loader: 'source-map' }
     ]
